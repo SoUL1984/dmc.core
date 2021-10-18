4ec065688887a698db95f0102b1469104adc8b49
Pavel Loshkarev <loshkarevs@yandex.ru>
2021-10-19 00:29:16 +0400

    base

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
---
+++ src/users/users.model.ts
@@ -0,0 +1,34 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+
+interface UserCreationAttrs {
+  email: string;
+  password: string;
+  phone: string;
+  role: number;
+}
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttrs> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: true })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: true })
+  password: string;
+
+  @Column({ type: DataType.STRING, unique: true, allowNull: true })
+  phone: string;
+
+  @Column({ type: DataType.INTEGER, allowNull: true })
+  role: number;
+
+  @Column({ type: DataType.DATE })
+  lastVisit: Date;
+}
