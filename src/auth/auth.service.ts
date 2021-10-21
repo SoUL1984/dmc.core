45f4ed32ae82a59b9fa4333ef2042368d6cf8075
Pavel Loshkarev <loshkarevs@yandex.ru>
2021-10-22 00:57:50 +0400

    Auth #3

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService);
+  constructor(private userService: UserService, private jwtService: JWTService);
   async login(userDto: CreateUserDto) {}
 
   async registration(userDto: CreateUserDto) {}
