7bf628c21215972517fa5bfe69f9e303432d931b
Pavel Loshkarev <loshkarevs@yandex.ru>
2021-10-23 00:12:05 +0400

    Auth ok

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,26 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import * as bcrypt from 'bcryptjs';
 import { User } from 'src/users/users.model';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService, private jwtService: JWTService);
-  async login(userDto: CreateUserDto) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(userDto: CreateUserDto) {
+    const user = await this.validateUser(userDto);
+    return this.generateToken(user);
+  }
 
   async registration(userDto: CreateUserDto) {
     const candidate = await this.userService.getUserByEmail(userDto.email);
@@ -15,18 +29,31 @@ export class AuthService {
         'Пользователь с таким email существует',
         HttpStatus.BAD_REQUEST,
       );
-      const hashPassword = await bcrypt.hash(userDto.password, 5);
-      const user = await this.userService.createUser({
-        ...userDto,
-        hashPassword,
-      });
-      return this.generateToken(user);
     }
+    const hashPassword = await bcrypt.hash(userDto.password, 5);
+    const user = await this.userService.createUser({
+      ...userDto,
+      password: hashPassword,
+    });
+    return this.generateToken(user);
   }
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payload = { email: user.email, id: user.id, role: user.role };
     return {
       token: this.jwtService.sign(payload),
     };
+  }
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({
+      message: 'Некорректный email или пароль',
+    });
   }
 }
