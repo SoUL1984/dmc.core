e0fd2b0be75452a2a8f80c73c4bd2329c1404550
NamaK-github <pavel.loshkarev@redium.co.nz>
2023-01-15 16:15:08 +0400

    tetst server

--- sh/delete_all_and_start_dmc_core.sh
+++ sh/delete_all_and_start_dmc_core.sh
@@ -8,6 +8,8 @@ cd ..
 git pull
 echo y | docker system prune
 echo y | docker system prune -a
+echo y | docker volume prune
+echo y | docker network prune
 docker-compose build
 docker-compose up
 exit
--- sh/delete_all_dmc_core.sh
+++ sh/delete_all_dmc_core.sh
@@ -7,6 +7,8 @@ echo "======================================="
 cd ..
 echo y | docker system prune
 echo y | docker system prune -a
+echo y | docker volume prune
+echo y | docker network prune
 docker images -a
 echo y | docker rmi image image
 exit
